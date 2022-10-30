0a8d6aa6d7ab0bf329a2784e40ac448f6163141d
Naveen Bakana <naveen.bakana@gmail.com>
2022-10-30 15:09:58 +0530

    heroku website hosting

---
+++ public/js/ImageScroll_cust.js
@@ -0,0 +1,12 @@
+$('#imgs').imageScroll({
+	orientation:"left",
+	speed:800,
+	interval:2000,
+	hoverPause:false,
+//	callback:function(){
+//	var ordinal = $(this).find("img").first().attr("src");
+//	<!-- console.log(ordinal); -->
+//	$(this).next("span").text("CallbackDisplay: hidden "+ordinal+"!");
+//	}
+});
+
---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Public static path
+const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname,"../templates/partials");
+
+app.set('view engine','hbs');
+app.set('views',template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+
+//routing
+//app.get("",(req, res)=>{
+//    res.render("website");
+//});
+
+app.get("",(req, res)=>{
+    res.render("about");
+});
+
+app.get("/products",(req, res)=>{
+    res.render("products");
+});
+
+app.get("/COUNTER",(req, res)=>{
+    res.render("products_counter");
+});
+
+app.get("/MALL",(req, res)=>{
+    res.render("products_mall_rack");
+});
+
+app.get("/MEDICAL",(req, res)=>{
+    res.render("products_medical");
+});
+
+app.get("/PILLAR",(req, res)=>{
+    res.render("products_pillar");
+});
+
+app.get("/SLOTTEDANGLE",(req, res)=>{
+    res.render("products_slotted_angle");
+});
+
+app.get("/contactus",(req, res)=>{
+    res.render("contactus");
+});
+
+app.get("*",(req, res)=>{
+    res.render("404error", {
+        errorMsg: ""
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`listening port number ${port}`);
+})
